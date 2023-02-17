a2b6a368479ac9d976b9f512e9de4f1f40442e7e
Subhash Singh <subhash_singh@seologistics.com>
2023-02-17 12:28:34 +0530

    a

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -7,4 +7,4 @@ cd /home/ec2-user/server
 #sudo pm2 delete Frontend
 #sudo pm2 start server.js --name Frontend
 pm2 delete Frontend
-pm2 start server.js --name Frontend
+pm2 start /home/ec2-user/server/server.js --name Frontend
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> YoYo :-)
+          Edit <code>src/App.js</code> Last Modified Date 17-Feb-23 :-)
         </p>
         <a
           className="App-link"
